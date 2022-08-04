167a0558f2ca60e88f33de6981db423c09d14d1d
Peera Arunrat <peelaxv@gmail.com>
2022-08-04 14:13:36 +0700

    finish

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 async function callApi() {
+  box.style.display = "none"
   const resp = await axios.get("https://randomuser.me/api/");
+  
+  }
+
+const button_random = document.getElementById("btn-random")
+const box = document.getElementById("div-user-card")
+
+button_random.onclick = () => {
+  
 }
+
+
+callApi();
+
+
+//ได้เเค่นี้ครับ ช่วยด้วย
